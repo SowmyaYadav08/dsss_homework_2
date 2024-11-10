21591b02c8ec7fa2833d9c78eaefc8c5e46fa46b
sowmya <sowmyayadav986@gmail.com>
2024-11-10 19:59:46 +0100

    Added the math_quiz.py and tests_math_quiz.py to git

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='dsss_homework_3',
-    version='',
-    packages=[''],
-    url='',
-    license='',
+    version='3.0',
+    packages= find_packages(),
+    url='https://github.com/SowmyaYadav08/dsss_homework_2',
+    license='GNU GENERAL PUBLIC LICENSE',
     author='sowmy',
-    author_email='',
+    author_email='sowmyayadav986@gmail.com',
     description=''
 )

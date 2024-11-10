26e17a669355e53d2e804f7632051799617788ad
sowmya <sowmyayadav986@gmail.com>
2024-11-10 19:47:09 +0100

    Adding a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsss_homework_3',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='sowmy',
+    author_email='',
+    description=''
+)
